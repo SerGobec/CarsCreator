bf4670112ed8280285acc99e15a8146a8f82e4fe
SerGObec <sergobec@gmail.com>
2021-04-23 13:34:08 +0300

    Created db

--- CarsCreator/Form1.Designer.cs
+++ CarsCreator/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace CarsCreator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.header = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(0, 0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(1006, 73);
+            this.header.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.header);
+            this.ForeColor = System.Drawing.Color.Cyan;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel header;
     }
 }
 
